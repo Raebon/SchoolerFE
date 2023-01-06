4b2443c81baba9f7c0da4cfafa1b23dd2b2a572a
Dominik Kučera <kuceradominik123@gmail.com>
2023-01-07 00:46:03 +0100

    refactor

--- src/pages/CreateQAPage/features/create-question-and-answer-list/createList.tsx
+++ src/pages/CreateQAPage/features/create-question-and-answer-list/createList.tsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react'
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { TextInput } from '../../../../components/form/inputs';
 import { QuestionAnswerBanner } from './components/questionAnswerBanner';
 import { Question } from './types';
 import { PrimaryButton } from '../../../../components/elements/buttons';
 
 const CreateList = () => {
-  const [showAllAnswers, setShowAllAnswers] = useState(false)
+  const [showAllAnswers, setShowAllAnswers] = useState(false);
   const { formState: { errors }, control, handleSubmit, reset } = useForm<{ question: string; answer: string }>();
-  const [questions, setQuestions] = React.useState<Question[]>([]);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
   const onSubmit = (data: { question: string; answer: string }) => {
     setQuestions([...questions, { id: questions.length, question: data.question, answer: data.answer }]);
     reset();
   };
 
-  const clearWords = () => setQuestions([])
+  const clearQuestions = () => setQuestions([]);
 
-  const clearWord = (wordId?: number) => {
-    return setQuestions(questions.filter(item => item.id !== wordId))
-  }
+  const clearQuestion = (questionId?: number) => {
+    setQuestions(questions.filter((item) => item.id !== questionId));
+  };
 
-  const handleShowAllAnswers = () => setShowAllAnswers(prev => !prev)
-
+  const handleShowAllAnswers = () => setShowAllAnswers((prev) => !prev);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -37,32 +36,30 @@ const CreateList = () => {
           <PrimaryButton type="submit">Přidat otázku</PrimaryButton>
         </div>
       </div>
-
-      {questions.length > 0 &&
-        <div className='flex justify-center mt-10'>
+      {questions.length > 0 && (
+        <div className="flex justify-center mt-10">
+          <PrimaryButton type="button" onClick={clearQuestions}>Vyčistit list</PrimaryButton>
           <PrimaryButton
             type="button"
-            onClick={clearWords}>Vyčistit list</PrimaryButton>
-          <PrimaryButton
-            type="button"
-            onClick={handleShowAllAnswers}>{showAllAnswers ? "Otevřít všechny otázky" : "Zavřít všechny otázky"}</PrimaryButton>
-          <PrimaryButton
-            type="button"
-            onClick={clearWords}>Uložit list</PrimaryButton>
+            onClick={handleShowAllAnswers}
+          >
+            {showAllAnswers ? 'Otevřít všechny odpovědi' : 'Zavřít všechny odpovědi'}
+          </PrimaryButton>
+          <PrimaryButton type="button" onClick={clearQuestions}>Uložit list</PrimaryButton>
         </div>
-      }
-
+      )}
       <div className="mb-10">
         {questions.map((item) => (
           <QuestionAnswerBanner
             key={`${item.id}-${new Date().toISOString()}`}
-            onRemoveClick={() => clearWord(item.id)}
+            onRemoveClick={() => clearQuestion(item.id)}
             question={item}
             toogleAllAnswers={showAllAnswers}
-          />))}
+          />
+        ))}
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default CreateList
+export default CreateList;
